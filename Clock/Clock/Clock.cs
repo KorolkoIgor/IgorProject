be458c7c6ec4023e68234da1f45ddc22799f8ea5
KorolkoIgor <korolko_igor@mail.ru>
2016-03-12 15:50:47 +0300

    second commit
    Signed-off-by: KorolkoIgor <korolko_igor@mail.ru>

---
+++ Clock/Clock/Clock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    public class Clock
+    {
+        public bool IsMechanic { get; set; }
+
+        public Clock(bool isMechanic)
+        {
+            IsMechanic = isMechanic;
+        }
+        public void Tick()
+        {
+            for(int i=0;i<60;i++)
+            {
+                if(IsMechanic)
+                {
+                    if (i % 10 == 0)
+                        Console.WriteLine("Machanic tick" + i);
+                }
+                else
+                {
+                    if (i % 3 == 0)
+                        Console.WriteLine("Electronic tick" + i);
+
+                }
+            }
+        }
+
+    }
+}
---
+++ Clock/Clock/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock mclock = new Clock(true);
+            mclock.Tick();
+
+            Clock eclock = new Clock(false);
+            eclock.Tick();
+
+            Console.Read();
+        }
+    }
+}
