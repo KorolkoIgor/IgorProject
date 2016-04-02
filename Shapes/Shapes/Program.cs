f499624edbb91b001d2e0c149bb261592ffe8d0f
KorolkoIgor <korolko_igor@mail.ru>
2016-04-02 12:42:43 +0300

    Signed-off-by: KorolkoIgor <korolko_igor@mail.ru>

---
+++ Shapes/Shapes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shape = {new Shape(),
+                                new Triangle(),
+                                new Circle(),
+                                new Quadrate(),
+                                new Rectangle(),
+                                new Trapezium()};
+
+            for (int i = 0; i < shape.Length; i++)
+            {
+                 shape[i].Draw();
+                 Console.WriteLine(shape[i].Square());
+            }
+            Console.WriteLine(Shape.Sum(shape));
+            //Sum.Shape(shape);
+            //Console.WriteLine(shape.Sum(Shape[] shape)); 
+        
+
+
+
+
+        }
+    }
+}
---
+++ Shapes/Shapes/Shape.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Shape
+    {
+        public string Color { get; set; }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine("фигура");
+        }
+
+        //int result;
+        public virtual int Square()
+        {
+            int result = 1;
+            return result;
+        }
+        //int sum = 0;
+
+        public static int Sum(Shape[] shape)
+        {
+            int sum = 0;
+            for (int i = 0; i < shape.Length; i++)
+            {
+                sum +=shape[i].Square();
+            }
+            return sum;
+
+        }
+
+    }
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("треугольник");
+        }
+        public override int Square()
+        {
+            int result = 2;
+            return result;
+        }
+
+
+    }
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("круг");
+        }
+        public override int Square()
+        {
+            int result = 3;
+            return result;
+        }
+
+
+    }
+    public class Quadrate : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("квадрат");
+        }
+        public override int Square()
+        {
+            int result = 4;
+            return result;
+        }
+
+
+    }
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("прямоугольник");
+        }
+        public override int Square()
+        {
+            int result = 5;
+            return result;
+        }
+
+
+    }
+
+    public class Trapezium : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("трапеция");
+        }
+        public override int Square()
+        {
+            int result = 6;
+            return result;
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+}
